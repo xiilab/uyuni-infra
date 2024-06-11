08f1727be849ea75ac6e418114038fa99c1533ea
moonsub <m.kwon@xiilab.com>
2024-06-11 16:06:07 +0900

    kubespray offline deploy script update

--- kubespray/deploy-kubespray.sh
+++ kubespray/deploy-kubespray.sh
@@ -6,12 +6,9 @@ source ~/.venv/3.11/bin/activate
 
 pip install -r $CURRENT_DIR/requirements.txt
 
-echo -n "Enter password: "
+echo -n "Enter Node's root password: "
 read -s password
-echo "$password" > ~/.vault_pass.txt
-chmod 600 ~/.vault_pass.txt
-ansible-vault encrypt ~/.vault_pass.txt
 
-ansible-playbook -i inventory/offline/astrago.yaml --ask-vault-pass=~/.vault_pass.txt offline-repo.yml
-ansible-playbook -i inventory/offline/astrago.yaml  --become --become-user=root --ask-vault-pass=~/.vault_pass.txt cluster.yml
+ansible-playbook -i inventory/offline/astrago.yaml --ask-vault-pass=~/.vault_pass.txt offline-repo.yml --extra-vars="ansible_password=$password"
+ansible-playbook -i inventory/offline/astrago.yaml  --become --become-user=root --ask-vault-pass=~/.vault_pass.txt cluster.yml --extra-vars="ansible_password=$password"
 
