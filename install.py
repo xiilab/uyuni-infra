543cc885539be0bf392713619f81aae1a96b87a9
root <root@agent1>
2024-06-14 07:33:21 +0000

    install python update

--- install.py
+++ install.py
@@ -1,5 +1,8 @@
 import curses
+import os
 import subprocess
+
+import yaml
 
 nodes = []
 
@@ -27,8 +30,8 @@ def print_nodes(stdscr):
     x = 30
     y = 0
 
-    header = ["   No   ", "     Node Name     ", "   IP Address   ", "   Role   "]
-    line_num = len(header[0]) + len(header[1]) + len(header[2]) + len(header[3]) + len(header) + 1
+    header = ["   No   ", "     Node Name     ", "   IP Address   ", "   Role   ", " Etcd "]
+    line_num = len(header[0]) + len(header[1]) + len(header[2]) + len(header[3]) + len(header[4]) + len(header) + 1
     line = ''.center(line_num, '-')
 
     stdscr.addstr(y, x, line)
@@ -38,8 +41,13 @@ def print_nodes(stdscr):
     stdscr.addstr(y, x, line)
 
     for idx, row in enumerate(nodes):
-        new_row = [str(idx).center(len(header[0])), row[0].center(len(header[1])), row[1].center(len(header[2])),
-                   row[2].center(len(header[3]))]
+        new_row = [
+            str(idx).center(len(header[0])),
+            row[0].center(len(header[1])),
+            row[1].center(len(header[2])),
+            row[2].center(len(header[3])),
+            row[3].center(len(header[4]))
+        ]
         y += 1
         stdscr.addstr(y, x, "|" + "|".join(new_row) + "|")
     y += 1
@@ -51,16 +59,55 @@ def add_node(stdscr):
     h, w = stdscr.getmaxyx()
     x = w // 2
     y = h // 2
+
     stdscr.addstr(y + 0, x, "Node Name: ")
     name = stdscr.getstr(y + 0, x + 11, 20).decode('utf-8')
 
     stdscr.addstr(y + 1, x, "IP Address: ")
     ip = stdscr.getstr(y + 1, x + 12, 20).decode('utf-8')
 
-    stdscr.addstr(y + 2, x, "Role: ")
-    role = stdscr.getstr(y + 2, x + 6, 20).decode('utf-8')
+    role_options = ["Master", "Worker"]
+    etcd_options = ["Y", "N"]
 
-    nodes.append([name, ip, role])
+    selected_roles = [False, False]
+    etcd_idx = 0
+    role_idx = 0
+
+    while True:
+        stdscr.addstr(y + 2, x, "Role: ")
+        for idx, option in enumerate(role_options):
+            if selected_roles[idx]:
+                stdscr.addstr(y + 2, x + 7 + idx * 10, "[X] " + option, curses.color_pair(2) if idx == role_idx else 0)
+            else:
+                stdscr.addstr(y + 2, x + 7 + idx * 10, "[ ] " + option, curses.color_pair(2) if idx == role_idx else 0)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_RIGHT:
+            role_idx = (role_idx + 1) % len(role_options)
+        elif key == curses.KEY_LEFT:
+            role_idx = (role_idx - 1) % len(role_options)
+        elif key == ord(' '):
+            selected_roles[role_idx] = not selected_roles[role_idx]
+            if not any(selected_roles):
+                selected_roles[role_idx] = True  # Ensure at least one is selected
+        elif key in [10, 13]:  # Enter key
+            if any(selected_roles):
+                break
+
+    while True:
+        stdscr.addstr(y + 3, x, f"Etcd: {etcd_options[etcd_idx]}".ljust(w - x), curses.color_pair(2))
+        key = stdscr.getch()
+
+        if key == curses.KEY_RIGHT:
+            etcd_idx = (etcd_idx + 1) % len(etcd_options)
+        elif key == curses.KEY_LEFT:
+            etcd_idx = (etcd_idx - 1) % len(etcd_options)
+        elif key in [10, 13]:  # Enter key
+            break
+
+    role = ",".join([role_options[i] for i in range(len(role_options)) if selected_roles[i]])
+    nodes.append([name, ip, role, etcd_options[etcd_idx]])
 
 
 def print_sub_menu(stdscr, selected_row_idx, menu):
@@ -100,13 +147,67 @@ def setting_node_menu(stdscr):
 
 
 def read_and_display_output(process, stdscr):
-    # Poll process for new output until finished
+    h, w = stdscr.getmaxyx()
+    output_lines = []
     while True:
-        nextline = process.stdout.readline()
-        stdscr.addstr(stdscr.getyx()[0], 0, nextline)
-        stdscr.refresh()
-        if nextline == '' and process.poll() is not None:
+        output = process.stdout.readline()
+        if output:
+            output_lines.append(output.decode('utf-8'))
+            if len(output_lines) > h - 2:
+                output_lines.pop(0)
+            stdscr.clear()
+            for idx, line in enumerate(output_lines):
+                stdscr.addstr(idx, 0, line[:w-1])
+            stdscr.refresh()
+        if output == '' and process.poll() is not None:
             break
+
+
+def create_inventory_file():
+    inventory = {
+        'all': {
+            'hosts': {},
+            'children': {
+                'kube-master': {
+                    'hosts': {}
+                },
+                'kube-node': {
+                    'hosts': {}
+                },
+                'etcd': {
+                    'hosts': {}
+                },
+                'k8s-cluster': {
+                    'children': {
+                        'kube-master': None,
+                        'kube-node': None
+                    }
+                },
+                'calico-rr': {
+                    'hosts': {}
+                }
+            }
+        }
+    }
+
+    for node in nodes:
+        node_name, ip, role, etcd = node
+        host_info = {
+            'ansible_host': ip,
+            'ip': ip,
+            'access_ip': ip
+        }
+        inventory['all']['hosts'][node_name] = host_info
+        if 'Master' in role:
+            inventory['all']['children']['kube-master']['hosts'][node_name] = None
+        if 'Worker' in role:
+            inventory['all']['children']['kube-node']['hosts'][node_name] = None
+        if etcd == 'Y':
+            inventory['all']['children']['etcd']['hosts'][node_name] = None
+
+    os.makedirs('kubespray/inventory/mycluster', exist_ok=True)
+    with open('kubespray/inventory/mycluster/astrago.yaml', 'w') as file:
+        yaml.dump(inventory, file, default_flow_style=False)
 
 
 def install_kubernetes_menu(stdscr):
@@ -126,13 +227,20 @@ def install_kubernetes_menu(stdscr):
             if current_row == 0:
                 setting_node_menu(stdscr)
             elif current_row == 1:
+                create_inventory_file()
                 stdscr.clear()
                 stdscr.addstr(0, 0, "Installing Kubernetes...")
+
+                curses.noecho()
+                stdscr.addstr(1, 0, "Input Node's Password: ")
+                password = stdscr.getstr(1, 23, 20).decode('utf-8')
+                curses.echo()
                 stdscr.refresh()
+
                 # Popen으로 실시간 출력
-                process = subprocess.Popen(["sh", "kubespray/deploy-kubespray.sh"], stdout=subprocess.PIPE,
+                process = subprocess.Popen(["bash", "kubespray/deploy-kubespray.sh", password], stdout=subprocess.PIPE,
                                            stderr=subprocess.STDOUT,
-                                           text=True)
+                                           executable="/bin/bash")
                 read_and_display_output(process, stdscr)
 
                 stdscr.addstr(stdscr.getyx()[0] + 1, 0, "Press any key to return to the menu")
@@ -154,38 +262,36 @@ def install_nfs():
 
 
 def main(stdscr):
-    try:
-        curses.initscr()
-        curses.curs_set(False)
-        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
-        current_row = 0
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
+    current_row = 0
+
+    print_menu(stdscr, current_row)
+
+    while True:
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(main_menu) - 1:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            if current_row == 0:
+                install_kubernetes_menu(stdscr)
+            elif current_row == 1:
+                install_astrago()
+            elif current_row == 2:
+                install_nfs()
+            elif current_row == 3:
+                break  # Exit the program
+
+            stdscr.clear()
+            print_menu(stdscr, current_row)
+            continue
 
         print_menu(stdscr, current_row)
 
-        while True:
-            key = stdscr.getch()
-
-            if key == curses.KEY_UP and current_row > 0:
-                current_row -= 1
-            elif key == curses.KEY_DOWN and current_row < len(main_menu) - 1:
-                current_row += 1
-            elif key == curses.KEY_ENTER or key in [10, 13]:
-                if current_row == 0:
-                    install_kubernetes_menu(stdscr)
-                elif current_row == 1:
-                    install_astrago()
-                elif current_row == 2:
-                    install_nfs()
-                elif current_row == 3:
-                    break  # Exit the program
-
-                stdscr.clear()
-                print_menu(stdscr, current_row)
-                continue
-
-            print_menu(stdscr, current_row)
-    finally:
-        curses.endwin()
-
 
 curses.wrapper(main)
+
